5cd9155df288d777774da584b1f398f31296676e
Pavlo Mryhlotskyi <kozak.holota@gmail.com>
2023-01-18 00:33:50 +0200

    Cities initial list load from CSV file.

--- OLXFakedBackend/Models/Db/ShopDbContext.cs
+++ OLXFakedBackend/Models/Db/ShopDbContext.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Formats.Asn1;
+using System.Text;
+using ChoETL;
 using Microsoft.EntityFrameworkCore;
 
 namespace OLXFakedBackend.Models
@@ -18,26 +21,12 @@ namespace OLXFakedBackend.Models
         public ShopDbContext(DbContextOptions options) : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder) {
-            modelBuilder.Entity<City>().HasData(
-                new City { CityId = 1, Name = "Київ, Київська область" },
-                new City { CityId = 2, Name = "Львів, Львівська область" },
-                new City { CityId = 3, Name = "Житомир, Житомирська область" },
-                new City { CityId = 4, Name = "Чернігів, Чернігівська" },
-                new City { CityId = 5, Name = "Ужгород, Закарпатська область" },
-                new City { CityId = 6, Name = "Тернопіль, Тернопільська область" },
-                new City { CityId = 7, Name = "Івано-Франківськ, Іванофранківська область" },
-                new City { CityId = 8, Name = "Хмельницьк, Хмельницька область" },
-                new City { CityId = 9, Name = "Миколаїв, Миколаївська область" },
-                new City { CityId = 10, Name = "Одеса, Одеська область" },
-                new City { CityId = 11, Name = "Харків, Харківська область" },
-                new City { CityId = 12, Name = "Суми, Сумська область" },
-                new City { CityId = 13, Name = "Луцьк, Волинська область" },
-                new City { CityId = 14, Name = "Рівне, Рівненська область" },
-                new City { CityId = 15, Name = "Чернівці, Чернівецька область" },
-                new City { CityId = 16, Name = "Дніпро, Дніпропетровська область" },
-                new City { CityId = 17, Name = "Запоріжжя, Запорізька область" },
-                new City { CityId = 18, Name = "Полтава, Полтавська область" }
-                );
+            using (var reader = new ChoCSVReader("DeploymentData/UkrainianCities.csv").WithFirstLineHeader()) {
+                foreach (dynamic item in reader)
+                {
+                    modelBuilder.Entity<City>().HasData(new City { CityId = int.Parse(item.ID), Name = item.NAME });
+                }
+            }
         }
     }
 }
