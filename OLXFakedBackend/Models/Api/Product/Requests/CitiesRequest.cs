0d0f06ae4bd420e0e994a7510de3a3192f398a5c
Pavlo Mryhlotskyi <kozak.holota@gmail.com>
2023-03-12 22:48:31 +0200

    Fix Cities Controller to get all cities

--- OLXFakedBackend/Models/Api/Product/Requests/CitiesRequest.cs
+++ OLXFakedBackend/Models/Api/Product/Requests/CitiesRequest.cs
@@ -3,7 +3,7 @@ namespace OLXFakedBackend.Models.Api.Product.Requests
 {
 	public class CitiesRequest: PaginatedRequest
     {
-		public string namePart { get; set; }
+		public string? namePart { get; set; }
 
     }
 }
